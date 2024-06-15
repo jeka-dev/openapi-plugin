9efbf26998e771e7351f24f81285224bbe1eecfc
Jerome Angibaud <djeangdev@yahoo.fr>
2024-06-15 22:32:36 +0200

    Migrate to JeKa 0.11

--- sample-prog/jeka-src/SampleProgBuild.java
+++ sample-prog/jeka-src/SampleProgBuild.java
@@ -1,36 +1,36 @@
 import dev.jeka.core.api.project.JkProject;
-import dev.jeka.core.tool.JkBean;
 import dev.jeka.core.tool.JkDoc;
 import dev.jeka.core.tool.JkInjectClasspath;
-import dev.jeka.plugins.openapi.OpenapiJkBean;
-import dev.jeka.plugins.springboot.SpringbootJkBean;
+import dev.jeka.core.tool.KBean;
+import dev.jeka.core.tool.builtins.project.ProjectKBean;
+import dev.jeka.plugins.openapi.JkOpenApiSourceGenerator;
+import dev.jeka.plugins.openapi.OpenapiKBean;
+import dev.jeka.plugins.springboot.SpringbootKBean;
 
-@JkInjectClasspath("../jeka/.work/def-classes")
+@JkInjectClasspath("../.jeka-work/jeka-src-classes")
 @JkInjectClasspath("dev.jeka:springboot-plugin")
-class SampleProgBuild extends JkBean {
+class SampleProgBuild extends KBean {
 
-    final SpringbootJkBean springbootBean = getBean(SpringbootJkBean.class);
+    final SpringbootKBean springbootBean = load(SpringbootKBean.class);
 
-    final OpenapiJkBean openapiKBean = getBean(OpenapiJkBean.class);
+    final OpenapiKBean openapiKBean = load(OpenapiKBean.class);
 
-    SampleProgBuild() {
-        springbootBean.setSpringbootVersion("3.1.4");
-        springbootBean.projectBean.lately(this::configure);
+    protected void init() {
         openapiKBean.cliVersion = "7.0.1";
-    }
-
-    private void configure(JkProject project) {
+        JkProject project = load(ProjectKBean.class).project;
         project.flatFacade()
-            .configureCompileDependencies(deps -> deps
+            .customizeCompileDeps(deps -> deps
+                    .and("org.springframework.boot:spring-boot-dependencies::pom:3.3.0")
                     .and("org.springframework.boot:spring-boot-starter-web")
                     .and("org.springdoc:springdoc-openapi-common:1.7.0")
                     .and("org.openapitools:jackson-databind-nullable:0.2.6")
             )
-            .configureTestDependencies(deps -> deps
+            .customizeTestDeps(deps -> deps
                     .and("org.springframework.boot:spring-boot-starter-test")
             );
-        openapiKBean.addSourceGenerator(project, "spring", "https://petstore.swagger.io/v2/swagger.json")
-                .customize(cmdBuilder -> cmdBuilder
+        JkOpenApiSourceGenerator sourceGenerator = JkOpenApiSourceGenerator.of("spring",
+                        "https://petstore.swagger.io/v2/swagger.json");
+        sourceGenerator.customize(cmdBuilder -> cmdBuilder
                         .add("--model-name-prefix", "Rest")
                         .addApiAndModelPackage("org.example.server")
                         .addAdditionalProperties("useSpringBoot3", "true")
@@ -38,11 +38,13 @@ class SampleProgBuild extends JkBean {
                         .addAdditionalProperties("useBeanValidation", "true")
                         .addAdditionalProperties("singleContentTypes", "true")
                 );
+        project.compilation.addSourceGenerator(sourceGenerator);
     }
 
     @JkDoc("Cleans, tests and creates bootable jar.")
     public void cleanPack() {
-        springbootBean.projectBean.cleanPack();
+        super.cleanOutput();
+        load(ProjectKBean.class).project.pack();
     }
 
 }
